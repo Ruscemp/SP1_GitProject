8addaa6502f51ed6f7703317b8218c0024ccde75
xWormx <carl_johan6@hotmail.com>
2023-09-08 14:38:41 +0200

    CALLE TAR ÖVER KOMMENTAREN
    min kommerntar

--- Assets/Scripts/PlayMovement.cs
+++ Assets/Scripts/PlayMovement.cs
@@ -8,7 +8,7 @@ public class PlayMovement : MonoBehaviour
 
     // He he he
     private bool canDoubleJump;
-    // Now it is my!
+    // CALLE
     Rigidbody2D rd2D;
 
     private bool betterJump = false;
